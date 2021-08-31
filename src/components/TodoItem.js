67dd1b9c176aa9b8ad8669b9232975b38a6d8963
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-31 13:42:12 +0200

    Use Object destructuring

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,21 +8,18 @@ function TodoItem(props) {
     opacity: 0.4,
     textDecoration: "line-through",
   }
-  
+
+  const { completed, id, title } = props.todo
   return (
     <li className={styles.item}>
       <input
         type="checkbox"
         className={styles.checkbox}
-        checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
       />
-      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-        Delete
-      </button>
-      <span style={this.props.todo.completed ? completedStyle : null}>
-        {this.props.todo.title}
-      </span>
+      <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
     </li>
   )
 }

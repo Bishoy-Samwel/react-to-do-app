037f22d00ba80d45ef736c5c0922b60554c8d1a2
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-08-31 22:50:08 +0200

    Change Project structre seperate class from function components

--- src/components/classBased/TodoContainer.js
+++ src/components/classBased/TodoContainer.js
@@ -1,28 +1,12 @@
 import React from "react"
-import TodosList from "./TodosList";
-import Header from "./Header"
+import TodosList from "../functionBased/TodosList";
+import Header from "../functionBased/Header"
 import InputTodo from "./InputTodo"
 import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
-  state =  {
-    todos: JSON.parse(localStorage.getItem("todos")) || [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
+  state = {
+    todos: JSON.parse(localStorage.getItem("todos")) || []
   };
 
   handleChange = id => {
@@ -39,7 +23,6 @@ class TodoContainer extends React.Component {
         }),
       }
     })
-    localStorage.setItem("todos", JSON.stringify(this.state.todos))
   };
 
   delTodo = id => {
@@ -51,7 +34,6 @@ class TodoContainer extends React.Component {
       ]
     });
     console.log(this.state.todos);
-    localStorage.setItem("todos", JSON.stringify(this.state.todos))
   };
 
   addTodoItem = title => {
@@ -63,7 +45,6 @@ class TodoContainer extends React.Component {
     this.setState({
       todos: [...this.state.todos, newTodo]
     });
-    localStorage.setItem("todos", JSON.stringify(this.state.todos))
   };
 
   setUpdate = (updatedTitle, id) => {
@@ -75,9 +56,28 @@ class TodoContainer extends React.Component {
         return todo
       }),
     })
-    localStorage.setItem("todos", JSON.stringify(this.state.todos))
   }
 
+  componentDidMount() {
+    if (!this.state.todos) {
+      fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+        .then(response => response.json())
+        .then(data => this.setState({ todos: data }));
+    }
+    // const temp = localStorage.getItem("todos")
+    // const loadedTodos = JSON.parse(temp)
+    // if (loadedTodos) {
+    //   this.setState({
+    //     todos: loadedTodos
+    //   })
+    // }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
   render() {
     return (
       <div className="container">

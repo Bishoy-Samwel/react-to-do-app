4fc22380dad7e1b65043298c6899624861cf4b1e
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-09-01 16:05:46 +0200

    Add Routert to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 //component file
 // import TodoContainer from "./components/classBased/TodoContainer"
 import TodoContainer from "./components/functionBased/TodoContainer"
@@ -8,7 +9,9 @@ import "./App.css"
 
 ReactDOM.render(
   <React.StrictMode>
+   <Router>
     <TodoContainer />
+   </Router>
   </React.StrictMode>,
   document.getElementById("root")
 )
